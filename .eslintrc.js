c9b40d1633b81d3b5c3b20f12c523c51f57882e0
Rai Butera <rai@rbutera.com>
2020-03-09 16:50:36 +0000

    update package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,10 +15,10 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:react/recommended",
   ],
 
   rules: {
