b61bc1922bcebfdeeabc9f1939d8b90662baf01b
Rai Butera <rai@rbutera.com>
2020-03-10 17:17:49 +0000

    update eslintrc and yarn.lock for xo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,16 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-    project: "./tsconfig.json",
+  settings: {
+    version: "detect",
   },
-  plugins: ["@typescript-eslint", "eslint-plugin-react"],
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-  ],
-
+  extends: ["xo/browser", "xo/esnext", "xo-typescript/space", "xo-react/space"],
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/camelcase": "off",
-    "@typescript-eslint/require-await": "off",
-  },
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      rules: {
-        "@typescript-eslint/explicit-function-return-type": ["error"],
-        "@typescript-eslint/member-delimiter-style": [
-          "error",
-          {
-            multiline: {
-              delimiter: "none",
-              requireLast: false,
-            },
-            singleline: {
-              delimiter: "semi",
-              requireLast: false,
-            },
-          },
-        ],
+    "@typescript-eslint/semi": 0,
+    "@typescript-eslint/restrict-template-expressions": 0,
+    "import/no-unassigned-import": [
+      "error",
+      {
+        allow: ["**/*.css"],
       },
-    },
-  ],
+    ],
+  },
 }
